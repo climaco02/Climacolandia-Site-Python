abc7fa3d5624f8795d13e5b20335f798b4858bd0
climaco02 <gabrielsclimaco@gmail.com>
2016-11-14 19:10:50 -0200

    adding urls for make order and show contact page

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from orders import views
+from views import MakeOrder
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^pedir/$', MakeOrder.as_view(), name='order'),
+	url(r'^contato/$', views.call, name='call')
 )
