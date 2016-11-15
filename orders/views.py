c038fc3b3394c8c09bc7bffe1e3d911b3ca1128a
climaco02 <gabrielsclimaco@gmail.com>
2016-11-15 00:27:43 -0200

    adding view methods that show and order, exclude it or mark it as completed

--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from orders.forms import MakeOrderForm
-from orders.models import Order
+from orders.models import Order, OrdersConcluded
 
 def index(request):
 	return render(request, 'index.html')
 
 def call(request):
 	return render(request, 'call.html')
+
+def manage(request):
+	return render(request, 'manage.html', { 'orders' : Order.objects.all(), 'orders_concluded' : OrdersConcluded.objects.all() })
+
+def conclude(request, order_id):
+	order = Order.objects.get(id=order_id)
+	order.conclude()
+	return redirect('manage')
+
+def show(request, order_id):
+	order = Order.objects.get(id=order_id)
+	already_concluded = order in OrdersConcluded.objects.all()
+	return render(request, 'show.html', { 'order' : order , 'already_concluded' : already_concluded })
+
+def show_concluded(request, order_id):
+	order = OrdersConcluded.objects.get(id=order_id)
+	already_concluded = order in OrdersConcluded.objects.all()
+	return render(request, 'show.html', { 'order' : order , 'already_concluded' : already_concluded })
+
+
+def exclude(request, order_id):
+	order = Order.objects.get(id=order_id)
+	order.exclude()
+	return redirect('manage')
 
 class MakeOrder(View):
 	template_name = 'make_order.html'
