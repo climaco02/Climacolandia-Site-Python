6e186073a241cc1c7c5248a7703d8c73fe300d95
climaco02 <gabrielsclimaco@gmail.com>
2016-11-15 00:26:23 -0200

    adding urls for managing orders

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,10 @@ from views import MakeOrder
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^pedir/$', MakeOrder.as_view(), name='order'),
-	url(r'^contato/$', views.call, name='call')
+	url(r'^contato/$', views.call, name='call'),
+	url(r'^gerenciar/$', views.manage, name='manage'),
+	url(r'^pedido/(?P<order_id>\d+)&', views.show, name='show'),
+	url(r'^pedido_concluido/(?P<order_id>\d+)&', views.show_concluded, name='show_concluded'),
+	url(r'^conclude/(?P<order_id>\d+)&', views.conclude, name='conclude'),
+	url(r'^exclude/(?P<order_id>\d+)&', views.exclude, name='exclude')
 )
