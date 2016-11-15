b7633780cba7dec835c7a3b91115677ac96b6e3d
climaco02 <gabrielsclimaco@gmail.com>
2016-11-15 00:25:11 -0200

    creating class to handle completed orders and creating methods to exclude a order or settle it as complete

--- orders/models.py
+++ orders/models.py
@@ -3,3 +3,16 @@ from django.db import models
 class Order(models.Model):
 	type_sweet = models.CharField(max_length=255, null=False)
 	description = models.TextField(null=False)
+
+	def conclude(self):
+		order_concluded = OrdersConcluded(type_sweet=self.type_sweet, 
+			description=self.description)
+		order_concluded.save()
+		self.delete()
+
+	def exclude(self):
+		self.delete()
+
+class OrdersConcluded(models.Model):
+	type_sweet = models.CharField(max_length=255, null=False)
+	description = models.TextField(null=False)
