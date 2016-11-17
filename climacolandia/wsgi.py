bfc0f0da6b6b23ffaf8661d4a352858d3a696e15
climaco02 <gabrielsclimaco@gmail.com>
2016-11-17 12:40:02 -0200

    configurating heroku

--- climacolandia/wsgi.py
+++ climacolandia/wsgi.py
@@ -7,6 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "climacolandia.settings")
 
